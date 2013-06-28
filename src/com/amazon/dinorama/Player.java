a92f17c2d888eebbd8e8cec4f146c573314a0310
Mark Mevorah <mmevorah@umich.edu>
2013-06-28 15:40:49 -0700

    Returns a rect for position now

--- src/com/amazon/dinorama/Player.java
+++ src/com/amazon/dinorama/Player.java
@@ -1,37 +1,59 @@
 package com.amazon.dinorama;
 
+import android.graphics.Rect;
+
 public class Player {
 	private int health = 100;
 	private int dinoType = 0;
 	
 	private boolean facingRight = true;
-	private double xPosition;
-	private double yPosition;
 	
-	private final long speed = 5;
+	private int width = 50;
+	private int height = 50;
+	private Rect positionBox;	
+	
+	private final int speed = 5;
 	
 	public Player(int dinoType, 
 				boolean facingRight,
-				double xPosition,
-				double yPosition){
+				int originX,
+				int originY){
+		
+		//Origin is centerX, centerY
+		int widthOffSet = width/2;
+		int heightOffSet = height/2;
+		
+		int left = originX-widthOffSet;
+		int right = originX+widthOffSet;
+		int up = originY+heightOffSet;
+		int down = originY-heightOffSet;
+		
+		Rect sizeAndPosition = new Rect(left, up, right, down);
+		
 		this.facingRight = facingRight;
 		this.dinoType = dinoType;
-		this.xPosition = xPosition;
-		this.yPosition = yPosition;
+		this.positionBox = sizeAndPosition;
 	}
 	
 	public void moveRight(double deltaTime){
-		xPosition += speed*deltaTime;
+		int offset = (int) (speed*deltaTime);
+		positionBox.offset(offset, 0);		
 	}
 	
 	public void moveLeft(double deltaTime){
-		xPosition -= speed*deltaTime;
+		int offset = (int) (speed*deltaTime);
+		positionBox.offset(-offset, 0);		
 	}
 	
 	public void flipPlayer(){
 		facingRight = false;
 	}
 	
+	public Rect getPosition(){
+		return positionBox;
+	}
+	
+	//For attacks, getPosition() then add (for right) say 50 pixels ahead or for left 50 pixels behind
 	public PlayerActions attackHigh(){
 		return PlayerActions.HIGHATTACK;
 	}
