330b784665c53bf98f0330ebc317524dd26d0e3a
Mark Mevorah <mmevorah@umich.edu>
2013-06-28 20:48:15 -0700

    new bg

--- gen/com/amazon/dinorama/R.java
+++ gen/com/amazon/dinorama/R.java
@@ -11,10 +11,40 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int arrow_down=0x7f020000;
+        public static final int arrow_hi=0x7f020001;
+        public static final int arrow_left=0x7f020002;
+        public static final int arrow_lo=0x7f020003;
+        public static final int arrow_right=0x7f020004;
+        public static final int arrow_up=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int main_menu_background=0x7f020007;
+        public static final int stego_1=0x7f020008;
+        public static final int stego_2=0x7f020009;
+        public static final int stego_3=0x7f02000a;
+        public static final int stego_4=0x7f02000b;
+        public static final int stego_5=0x7f02000c;
+        public static final int stego_6=0x7f02000d;
+        public static final int stego_7=0x7f02000e;
+        public static final int test=0x7f02000f;
+    }
+    public static final class id {
+        public static final int buttonLoadPicture=0x7f050006;
+        public static final int imageButton1=0x7f050001;
+        public static final int imageButton2=0x7f050003;
+        public static final int imageButton3=0x7f050002;
+        public static final int imageButton4=0x7f050004;
+        public static final int imageView1=0x7f050000;
+        public static final int imgView=0x7f050005;
+        public static final int newArcadeButton=0x7f050008;
+        public static final int newCampaignButton=0x7f050007;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int character_select_screen=0x7f030000;
+        public static final int game_over_screen=0x7f030001;
+        public static final int level_select_screen=0x7f030002;
+        public static final int main=0x7f030003;
+        public static final int main_menu=0x7f030004;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
